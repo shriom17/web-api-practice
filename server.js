e92dbd7e9c7f048e71574d6e3630be2e15159a20
shriom17 <129147531+shriom17@users.noreply.github.com>
2026-09-20 12:51:08 +0530

    get api added

--- server.js
+++ server.js
@@ -7,3 +7,19 @@ app.get("/", (req, res) => {
 app.listen(3000, () =>{
     console.log("Server is running on Port 3000");
 });
+
+//GET
+const users = [
+    {id: 1, name: "John Doe"},  
+    {id: 2, name: "Jane Doe"},
+    {id: 3, name: "Bob Smith"},
+    {id: 4, name: "Alice Johnson"},
+    {id: 5, name: "Charlie Brown"}
+]
+
+app.get("/users", (req,res) => {
+    res.json(users);
+})
+app.get("/users/:id", (req,res) =>{
+    res.json(users.find(user => user.id === parseInt(req.params.id)));
+})
